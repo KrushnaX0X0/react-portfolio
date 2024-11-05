18e6aedb9aaf9a04ed1984886d2a7563091d01a5
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-11-05 20:49:50 +0530

    add responcive

--- src/Components/FileOpner/FileOpner.jsx
+++ src/Components/FileOpner/FileOpner.jsx
@@ -3,40 +3,42 @@ import "./FileOpner.css"
 import Nav from "../Nav/Nav"
 
 
-function FileOpner({data}) {
+function FileOpner({ data }) {
 
   return (
     <>
-    
+
 
       <div className='fileopner-continer'>
-        <Nav/>
-        
+        <Nav />
+
         <p className='title-name'>📄 {data.name}</p>
-        <div className='title-continer'>
-         {data.fildData.map((showdata ,index)=>{
-          console.log(showdata,index )
-           return(<>
-              <div key={index} >
-                <h2 className='title' key={index} >{showdata.title}</h2>
-                <p className=''>{showdata.discription}</p>
-                 <div>
-                  {showdata.Image == false ? null : <>
-                    {
-                      showdata.Image.map((path)=>(
-                        <img src={path} className='project-img'/>
-                      ))
-                    }
-                  </>}
-                 </div>
-              </div>
-            </>)
-         })}
-      </div>
+        <div className="open-file-continer">
+          <div className='title-continer'>
+            {data.fildData.map((showdata, index) => {
+              console.log(showdata, index)
+              return (<>
+                <div key={index} >
+                  <h2 className='title' key={index} >{showdata.title}</h2>
+                  <p className=''>{showdata.discription}</p>
+                  <div>
+                    {showdata.Image == false ? null : <>
+                      {
+                        showdata.Image.map((path) => (
+                          <img src={path} className='project-img' />
+                        ))
+                      }
+                    </>}
+                  </div>
+                </div>
+              </>)
+            })}
+          </div>
+        </div>
 
 
       </div>
-    
+
 
     </>
   )
